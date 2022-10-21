66fbf09e759ee1f666b3115204bfdd31956ab583
WALTER <walteranramirez@gmail.com>
2022-10-20 20:57:05 -0600

    commit
    add: en la pantalla de inicio se adjuntaron los datos a mostrar y consumo de api

--- lib/Paginas/Principal.dart
+++ lib/Paginas/Principal.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:parcial03/json/Data.dart';
 
 class PrincipalP03 extends StatefulWidget {
@@ -21,7 +20,7 @@ class _PrincipalP03State extends State<PrincipalP03> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBar(),
-      body: body(),
+      body: datos(),
     );
   }
 
@@ -47,60 +46,127 @@ class _PrincipalP03State extends State<PrincipalP03> {
       child: Column(
         children: [
           sliderDatos(),
+          datos()
         ],
       ),
     );
   }
 
+  Widget datos(){
+    return Stack(
+      children: [
+        sliderDatos(),
+        Positioned(
+          top: 100,
+          bottom: 0,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              peliculas.length != null?
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 1.4
+                  ), 
+                  itemCount: peliculas.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 4.0,
+                          horizontal: 8.0
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top: 20,
+                                left: 20,
+                                child: Text(
+                                  peliculas[index]['title'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13,
+                                    color: Colors.blueGrey
+                                  ),
+                                )
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ):
+              Center(
+                child: CircularProgressIndicator(),
+              )
+            ]
+          ),
+        )
+      ],
+    );
+  }
+
   Widget sliderDatos() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10),
-        child: Row(
-          children: List.generate(Datos.length, (index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: GestureDetector(
-                onTap: () {
-                  //
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(255, 227, 235, 238),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(2),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: (MediaQuery.of(context).size.width) -
-                              (MediaQuery.of(context).size.width) / 15,
-                          height: 100,
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 8),
-                              child: Text(
-                                "Alumno: " +
-                                    Datos[index]['name'] +
-                                    " " +
-                                    Datos[index]['lastname'] +
-                                    "\n" "Carnet: " +
-                                    Datos[index]['number'],
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
+    return Container(
+      //height: 150,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Row(
+            children: List.generate(Datos.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: GestureDetector(
+                  onTap: () {
+                    //
+                  },
+                  child: Container(
+                    //height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      //color: Color.fromARGB(255, 227, 235, 238),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: Column(
+                        children: [
+                          Container(
+                            color: Color.fromARGB(255, 227, 235, 238),
+                            width: (MediaQuery.of(context).size.width) -
+                                (MediaQuery.of(context).size.width) / 15,
+                            height: 100,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 8),
+                                child: Text(
+                                  "Alumno: " +
+                                      Datos[index]['name'] +
+                                      " " +
+                                      Datos[index]['lastname'] +
+                                      "\n" "Carnet: " +
+                                      Datos[index]['number'],
+                                  style: TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          }),
+              );
+            }),
+          ),
         ),
       ),
     );
