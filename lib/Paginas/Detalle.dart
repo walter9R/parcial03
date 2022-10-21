85bd7ce66829fc407add452288fda938d53c762a
WALTER <walteranramirez@gmail.com>
2022-10-20 22:48:47 -0600

    commit
    fix: detalle pixeleado

--- lib/Paginas/Detalle.dart
+++ lib/Paginas/Detalle.dart
@@ -21,9 +21,84 @@ class _DetalleP03State extends State<DetalleP03> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(213, 229, 240, 240),
       appBar: appBar(),
-      body: body(),
+      body: datos(),
+    );
+  }
+
+  Widget datos(){
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    return Stack(
+      children: [
+        Positioned(
+          top: 10,
+          bottom: 0,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              widget.gamedetalle.length != null?
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1, childAspectRatio: 0.75
+                  ), 
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 4.0,
+                          horizontal: 8.0
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            //color: Colors.blueAccent,
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top: 5,
+                                child: CachedNetworkImage(
+                                  imageUrl: widget.gamedetalle['thumbnail'],
+                                  width: MediaQuery.of(context).size.width,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                top: MediaQuery.of(context).size.width /1.7,
+                                left: 8,
+                                child: Text(
+                                  "Titulo: " + widget.gamedetalle['title'] + "\n" +"\n" 
+                                  "Genero: " + widget.gamedetalle['genre'] + "\n" +"\n"
+                                  "Plataforma: " + widget.gamedetalle['platform'] + "\n" +"\n"
+                                  "Publicador: " + widget.gamedetalle['publisher'] + "\n" +"\n"
+                                  "Desarrolladora: " + widget.gamedetalle['developer'] + "\n" +"\n"
+                                  "Lanzamiento: " +widget.gamedetalle['release_date'] + "\n" +"\n"
+                                  , 
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.black
+                                  ),
+                                )
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ):
+              Center(
+                child: CircularProgressIndicator(),
+              )
+            ]
+          ),
+        )
+      ],
     );
   }
 
@@ -57,188 +132,5 @@ class _DetalleP03State extends State<DetalleP03> {
     );
   }
 
-  body() {
-    var ancho = MediaQuery.of(context).size.width;
-    var alto = MediaQuery.of(context).size.height;
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Positioned(
-          top: 80,
-          left: 120,
-          child: Text(
-            widget.gamedetalle['title'],
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 32),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            width: ancho,
-            height: alto * 0.6,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child:
-                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Nombre: ",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['title'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Genero: ",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['genre'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Plataforma: ",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['platform'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Publicador: ",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['publisher'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Desarrollador: ",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['developer'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              "Lanzamiento:",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 16),
-                            )),
-                        Container(
-                            width: ancho * 0.3,
-                            child: Text(
-                              widget.gamedetalle['release_date'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ]),
-                ),
-              ]),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 25,
-          child: CachedNetworkImage(
-              imageUrl: widget.gamedetalle['thumbnail'],
-              height: 170,
-              fit: BoxFit.fitHeight),
-        )
-      ],
-    );
-  }
+  
 }
